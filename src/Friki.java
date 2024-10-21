416033b2e13c47e9c73202dd956f4eaf4b33b56b
paula <paulayaqueline.benitez@gracia.lasalle.cat>
2024-10-21 09:08:05 +0200

    Primer commit - Inicialización del Proyecto

---
+++ src/Friki.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int segundo = sc.nextInt();
+        String temps;
+        int dia = (segundo / 86400) + 1;
+        int segundosRestantes = (segundo % 86400);
+        if (segundosRestantes < 43200){
+            temps = "mati ";
+        } else {
+            temps = "nit";
+        }
+        System.out.println(temps+" del dia "+dia);
+    }
+}
